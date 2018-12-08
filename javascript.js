7a85c2c7e3d5cb89c0f1953ae8870539360dee15
lan-mi <milan221088@yahoo.com>
2018-12-08 21:45:33 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById('box');
+    if (x.style.backgroundColor === 'red') {
+        x.style.backgroundColor = 'blue';
+    } else {
+        x.style.backgroundColor = 'red';
+    }
+}
